fedf62e5d928ae0ca5cfc21a354782b71af7e0de
ndaruyudha <ndaruyudha@gmail.com>
2024-04-17 12:49:16 +0700

    new file:   opr/CaseStudy1/TipePembayaran.php 	new file:   opr/CaseStudy1/cash.php 	new file:   opr/CaseStudy1/customerPay.php 	new file:   opr/CaseStudy1/debit.php 	new file:   opr/CaseStudy1/index.php 	new file:   opr/CaseStudy1/kredit.php 	new file:   opr/CaseStudy1/opr_cs1.png 	new file:   opr/CaseStudy2/dbms.php 	new file:   opr/CaseStudy2/dbmsconnect.php 	new file:   opr/CaseStudy2/index.php 	new file:   opr/CaseStudy2/mongodb.php 	new file:   opr/CaseStudy2/mysql.php 	new file:   opr/CaseStudy2/opr_cs2.png

---
+++ opr/CaseStudy1/index.php
@@ -0,0 +1,17 @@
+<?php
+include_once "customerPay.php";
+include_once "debit.php";
+include_once "kredit.php";
+include_once "cash.php";
+
+$payment = new PembayaranCustomer();
+$debit = new Debit();
+$kredit = new Kredit();
+$cash = new Cash();
+
+$payment->menerimaPembayaran($debit);
+echo "<br>";
+$payment->menerimaPembayaran($kredit);
+echo "<br>";
+$payment->menerimaPembayaran($cash);
+?>
